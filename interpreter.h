6fdec36ca5e2aa01f19b1c17206ddb630cdab62d
Gareth618 <bloggareth@gmail.com>
2022-01-12 02:45:41 +0200

    almost finished dfs

--- interpreter.h
+++ interpreter.h
@@ -283,6 +283,273 @@ namespace Pickle {
                 dfs(function, nullptr);
         }
 
+        void checkForTypeErrors() {
+            function<string(Node)> dfs = [&](Node node) -> string {
+                auto check = [&](string type, ObjectLiteral* objectLiteral) {
+                    if (!objectMap.count(type))
+                        return false;
+                    auto object = objectMap[type];
+                    auto& [members] = *objectLiteral;
+                    for (auto [name, value] : members) {
+                        const string _name = name;
+                        auto it = find_if(object->members.begin(), object->members.end(), [&](pair<string, string> member) {
+                            return member.second == _name;
+                        });
+                        if (it == object->members.end())
+                            return false;
+                        if (it->first != dfs(value))
+                            return false;
+                    }
+                    return true;
+                };
+
+                if (node.index() == 1) {
+                    auto function = get<1>(node);
+                    auto& [type, name, arguments, statements] = *function;
+                    for (auto statement : statements)
+                        dfs(statement);
+                    return "";
+                }
+                if (node.index() == 2) {
+                    auto declaration = get<2>(node);
+                    auto& [type, name, value, constant] = *declaration;
+                    const string valueType = dfs(value);
+                    if (type.back() == ']' && valueType == "$array")
+                        return "";
+                    if (value->content.index() == 1 && get<1>(value->content)->content.index() == 6 && check(type, get<6>(get<1>(value->content)->content)))
+                        return "";
+                    if (type != valueType)
+                        errors.push_back("variable " + green(name) + " is of type " + green(type) + " and is assigned an expression of type " + green(valueType));
+                    return "";
+                }
+                if (node.index() == 3) {
+                    auto assignation = get<3>(node);
+                    auto& [variable, value, op] = *assignation;
+                    const string variableType = dfs(variable);
+                    const string valueType = dfs(value);
+                    if (variableType.back() == ']' && valueType == "$array")
+                        return "";
+                    if (value->content.index() == 1 && get<1>(value->content)->content.index() == 6 && check(variableType, get<6>(get<1>(value->content)->content)))
+                        return "";
+                    if (variableType != valueType)
+                        errors.push_back("lvalue is of type " + green(variableType) + " but rvalue is of type " + green(valueType));
+                    return "";
+                }
+                if (node.index() == 4) {
+                    auto _if = get<4>(node);
+                    auto& [conditions, statements] = *_if;
+                    int index = 0;
+                    for (auto condition : conditions) {
+                        const string conditionType = dfs(condition);
+                        if (conditionType != "bool")
+                            errors.push_back("expression inside " + green(index ? "elif" : "if") + " is of type " + green(conditionType));
+                        index++;
+                    }
+                    for (auto& group : statements)
+                        for (auto statement : group)
+                            dfs(statement);
+                    return "";
+                }
+                if (node.index() == 5) {
+                    auto _while = get<5>(node);
+                    auto& [condition, statements] = *_while;
+                    const string conditionType = dfs(condition);
+                    if (conditionType != "bool")
+                        errors.push_back("expression inside " + green("while") + " is of type " + green(conditionType));
+                    for (auto statement : statements)
+                        dfs(statement);
+                    return "";
+                }
+                if (node.index() == 6) {
+                    auto _for = get<6>(node);
+                    auto& [iterator, from, to, step, statements] = *_for;
+                    const string fromType = dfs(from);
+                    const string toType = dfs(to);
+                    const string stepType = dfs(step);
+                    if (fromType != "int")
+                        errors.push_back(green("from") + " expression inside " + green("for") + " is of type " + green(fromType));
+                    if (toType != "int")
+                        errors.push_back(green("to") + " expression inside " + green("for") + " is of type " + green(toType));
+                    if (stepType != "int")
+                        errors.push_back(green("step") + " expression inside " + green("for") + " is of type " + green(stepType));
+                    for (auto statement : statements)
+                        dfs(statement);
+                    return "";
+                }
+                if (node.index() == 7) {
+                    auto memberAccess = get<7>(node);
+                    auto& [object, member] = *memberAccess;
+                    const string objectType = dfs(object);
+                    if (!objectMap.count(objectType)) {
+                        errors.push_back(green(objectType) + " is not a user-defined type");
+                        return "?";
+                    }
+                    for (auto [type, name] : objectMap[objectType]->members)
+                        if (name == member)
+                            return type;
+                    errors.push_back("member " + green(member) + " is not defined for type " + green(objectType));
+                    return "?";
+                }
+                if (node.index() == 8) {
+                    auto elementAccess = get<8>(node);
+                    auto& [array, index] = *elementAccess;
+                    const string arrayType = dfs(array);
+                    const string indexType = dfs(index);
+                    if (arrayType.back() != ']') {
+                        errors.push_back(green(arrayType) + " is not an array type");
+                        return "?";
+                    }
+                    if (indexType != "int") {
+                        errors.push_back(green(indexType) + " is not a subscript type");
+                        return "?";
+                    }
+                    return arrayType.substr(0, int(arrayType.size()) - 2);
+                }
+                if (node.index() == 9) {
+                    auto objectLiteral = get<9>(node);
+                    auto& [members] = *objectLiteral;
+                    set<string> memberNames;
+                    for (auto [name, value] : members) {
+                        if (memberNames.count(name)) {
+                            errors.push_back("redefinition of member " + green(name) + " inside object literal");
+                            return "?";
+                        }
+                        memberNames.insert(name);
+                    }
+                    return "$object";
+                }
+                if (node.index() == 10) {
+                    auto functionCall = get<10>(node);
+                    auto& [name, arguments] = *functionCall;
+                    string signature = name + "(";
+                    for (auto argument : arguments)
+                        signature += dfs(argument) + ", ";
+                    if (!arguments.empty()) {
+                        signature.pop_back();
+                        signature.pop_back();
+                    }
+                    signature += ")";
+                    if (!functionMap.count(signature)) {
+                        errors.push_back("there is no function of signature " + green(signature) + " to be called");
+                        return "?";
+                    }
+                    return functionMap[signature]->type;
+                }
+                if (node.index() == 11) {
+                    auto unaryExpression = get<11>(node);
+                    auto& [value, op] = *unaryExpression;
+                    const string valueType = dfs(value);
+                    if (op == "not") {
+                        if (valueType != "bool") {
+                            errors.push_back(green(op) + " expects a " + green("bool") + " operand");
+                            return "?";
+                        }
+                        return "bool";
+                    }
+                    return "?";
+                }
+                if (node.index() == 12) {
+                    auto binaryExpression = get<12>(node);
+                    auto& [lhs, rhs, op] = *binaryExpression;
+                    const string lhsType = dfs(lhs);
+                    const string rhsType = dfs(rhs);
+                    if (op == "and" || op == "or") {
+                        if (lhsType != "bool" || rhsType != "bool") {
+                            errors.push_back(green(op) + " expects operands of type " + green("bool"));
+                            return "?";
+                        }
+                        return "bool";
+                    }
+                    if (op == "==" || op == "<>") {
+                        if (lhsType != rhsType) {
+                            errors.push_back(green(op) + " expects operands of the same type");
+                            return "?";
+                        }
+                        return "bool";
+                    }
+                    if (op == "<" || op == "<=" || op == ">" || op == ">=") {
+                        if (lhsType != rhsType) {
+                            errors.push_back(green(op) + " expects operands of the same type");
+                            return "?";
+                        }
+                        if (lhsType != "int" && lhsType != "float") {
+                            errors.push_back(green(op) + " expects operands of type " + green("int") + " or " + green("float"));
+                            return "?";
+                        }
+                        return "bool";
+                    }
+                    if (op == "+" || op == "-" || op == "*" || op == "/") {
+                        if (lhsType != rhsType) {
+                            errors.push_back(green(op) + " expects operands of the same type");
+                            return "?";
+                        }
+                        if (lhsType != "int" && lhsType != "float") {
+                            errors.push_back(green(op) + " expects operands of type " + green("int") + " or " + green("float"));
+                            return "?";
+                        }
+                        return lhsType;
+                    }
+                    if (op == "%") {
+                        if (lhsType != "int" || rhsType != "int") {
+                            errors.push_back(green(op) + " expects operands of type " + green("int"));
+                            return "?";
+                        }
+                        return "int";
+                    }
+                    return "?";
+                }
+                if (node.index() == 13) {
+                    auto lvalue = get<13>(node)->content;
+                    if (lvalue.index() == 0) return dfs(get<0>(lvalue));
+                    if (lvalue.index() == 1) return dfs(get<1>(lvalue));
+                    if (lvalue.index() == 2) {
+                        return "?"; // TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                    }
+                }
+                if (node.index() == 14) {
+                    auto rvalue = get<14>(node)->content;
+                    if (rvalue.index() == 0) return dfs(get<0>(rvalue));
+                    if (rvalue.index() == 1) return dfs(get<1>(rvalue));
+                    if (rvalue.index() == 2) return dfs(get<2>(rvalue));
+                    if (rvalue.index() == 3) return dfs(get<3>(rvalue));
+                    if (rvalue.index() == 4) return dfs(get<4>(rvalue));
+                    if (rvalue.index() == 5) return dfs(get<5>(rvalue));
+                }
+                if (node.index() == 15) {
+                    auto literal = get<15>(node)->content;
+                    if (literal.index() == 0) return "int";
+                    if (literal.index() == 1) return "float";
+                    if (literal.index() == 2) return "char";
+                    if (literal.index() == 3) return "string";
+                    if (literal.index() == 4) return "bool";
+                    if (literal.index() == 5) {
+                        const string lengthType = dfs(get<5>(literal));
+                        if (lengthType != "int") {
+                            errors.push_back("array literal expects a length of type " + green("int"));
+                            return "?";
+                        }
+                        return "$array";
+                    }
+                    if (literal.index() == 6) return dfs(get<6>(literal));
+                }
+                if (node.index() == 16) {
+                    auto statement = get<16>(node)->content;
+                    if (statement.index() == 0) return dfs(get<0>(statement));
+                    if (statement.index() == 1) return dfs(get<1>(statement));
+                    if (statement.index() == 2) return dfs(get<2>(statement));
+                    if (statement.index() == 3) return dfs(get<3>(statement));
+                    if (statement.index() == 4) return dfs(get<4>(statement));
+                    if (statement.index() == 5) return dfs(get<5>(statement));
+                    if (statement.index() == 7) return dfs(get<7>(statement));
+                }
+                return "";
+            };
+            for (auto declaration : declarations)
+                dfs(declaration);
+            for (auto function : functions)
+                dfs(function);
+        }
+
         void createTables() {
             ofstream fout("symbols.txt");
             function<void(Node, int)> dfs = [&](Node node, int level) {
@@ -358,7 +625,7 @@ namespace Pickle {
             for (auto declaration : declarations)
                 dfs(declaration, 0);
 
-            fout << "\nUSER-DEFINED TYPES";
+            fout << "\n\nUSER-DEFINED TYPES";
             fout << "\n==================\n\n";
             for (auto object : objects) {
                 auto& [name, members] = *object;
@@ -368,7 +635,7 @@ namespace Pickle {
                 fout << "}\n";
             }
 
-            fout << "\nFUNCTIONS";
+            fout << "\n\nFUNCTIONS";
             fout << "\n=========\n\n";
             for (int i = 5; i < int(functions.size()); i++) {
                 auto& [type, name, arguments, statements] = *(functions[i]);
@@ -389,6 +656,7 @@ namespace Pickle {
             checkForCyclicDependencies();
             checkForBreakContinueErrors();
             checkForUndefinedTypeErrors();
+            checkForTypeErrors();
         }
 
     public:
